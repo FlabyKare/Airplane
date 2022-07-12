8e92d94e6d0b3320d0ae812fec8a4a273222fab6
FlabyKare <beslansoblirov074@gmail.com>
2022-07-12 14:59:06 +0300

    open_menu - block_scroll

--- js/index.js
+++ js/index.js
@@ -6,11 +6,11 @@ burg.click(function(){
         $('.span_burger2').toggleClass('hidden')
         $('.burger').toggleClass('transform_rotate')
         $('.menu_mobile').toggleClass('d-flex')
-
+        $('body').toggleClass('overflow_hidden')
         
     })
 
-
+// Привязываю переменным классы
 let 
 from_punkt1 = document.querySelector('.from_punkt1'),
 from_punkt2 = document.querySelector('.from_punkt2'),
@@ -18,6 +18,7 @@ from_punkt3 = document.querySelector('.from_punkt3'),
 from_punkt4 = document.querySelector('.from_punkt4'),
 btn = document.querySelector('.change_icon')
 
+// Создаю действия при клике на кнопку
 btn.addEventListener('click',function () {
 let text1 = from_punkt1.textContent;
 let text2 = from_punkt2.textContent; 
