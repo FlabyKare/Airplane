bf69dda6f40b92a254f09edfb5516094453644f6
FlabyKare <beslansoblirov074@gmail.com>
2022-07-14 15:08:52 +0300

    Adaptate_For_Apple_Devices

--- js/index.js
+++ js/index.js
@@ -59,10 +59,21 @@ from_punkt4.textContent = text3
 
 
 
-
-
-
-
-
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
 
 });
